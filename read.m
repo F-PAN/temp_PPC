5345238691aa138dfbc6ca74fdf5f83a9a1d22a1
Fengjunjie PAN <panfengjunjie@gmail.com>
2018-06-26 10:25:49 +0200

    Update read.m

--- read.m
+++ read.m
@@ -3,27 +3,30 @@
 clc;
 clear all;
 
+#load ../catkin_ws/src/PPC_data.dat
 load ../PPC_data.dat
-
 f=figure(1)
 set(gca,'linewidth',1.5);
 set(gca,'gridlinestyle','-');
-plot(PPC_data(:,1),PPC_data(:,2),'r')
+plot(PPC_data(:,1),PPC_data(:,3),'r')
 %plot(PPC_data(:,2),'r')
 hold;
-plot(PPC_data(:,1),PPC_data(:,3),'b')
+plot(PPC_data(:,1),PPC_data(:,2),'g')
 %plot(PPC_data(:,3),'r')
 title('force over time');
 xlabel('time [s]')
-xlim([0 5]);
+#xlim([0 5]);
 ylabel('force [N]')
 grid on;
 
 e=figure(2)
-set(gca,'linewidth',1.5);
-set(gca,'gridlinestyle','-');
-plot(PPC_data(:,5),PPC_data(:,6),'r')
+%set(gca,'linewidth',1.5);
+%set(gca,'gridlinestyle','.');
+scatter(PPC_data(:,1),PPC_data(:,7),'r')
 hold on;
+scatter(PPC_data(:,1),PPC_data(:,6),'g')
+hold on;
+scatter(PPC_data(:,1),PPC_data(:,5),'y')
 %plot(PPC_data(:,1),PPC_data(:,6),'b')
 title('performace bound and error ove rtime');
 xlabel('error [N]')
@@ -34,24 +37,36 @@ grid on;
 et=figure(3)
 set(gca,'linewidth',1.5);
 set(gca,'gridlinestyle','-');
-plot(PPC_data(:,1),PPC_data(:,4),'r')
+plot(PPC_data(:,1),PPC_data(:,4),'color',[0.5 0.5 0.5])
 %plot(PPC_data(:,4),'r')
 hold on;
-plot(PPC_data(:,1),-1*PPC_data(:,5),'b')
-%plot(PPC_data(:,5),'b')
-title('performace bound and error over time');
+%plot(PPC_data(:,1),-1*PPC_data(:,5),'r')
+%hold on;
+plot(PPC_data(:,1),-1*PPC_data(:,10),'g')
+hold on;
+#scatter(PPC_data(:,1),-1*PPC_data(:,5),'g')
+%title('performace bound and error over time');
+hold on; 
+line([0,20],[-1,-1],'color',[0.5 0.5 0.5]);
+title('tracking error over time');
 xlabel('time [s]')
-xlim([0 5]);
-ylabel('transformed error [N]')
+#xlim([0 5]);
+ylabel('error [N]')
+#ylim([-3 6])
 grid on;
+
 
 v=figure(4)
 set(gca,'linewidth',1.5);
 set(gca,'gridlinestyle','-');
-plot(PPC_data(:,1),PPC_data(:,8),'g')
+plot(PPC_data(:,1),PPC_data(:,8),'r')
+hold on;
 %plot(PPC_data(:,8),'g')
 xlabel('time [s]')
-xlim([0 5]);
+%xlim([0 5]);
 ylabel('velocity')
 
-
+figure(5)
+plot(PPC_data(:,9),PPC_data(:,6),'r')
+hold on;
+plot(PPC_data(:,9),PPC_data(:,7),'g')
